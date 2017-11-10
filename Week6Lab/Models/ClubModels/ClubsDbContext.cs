86331859617148a90ed03359db3f41f357b1c44d
s00161418 <s00161418@mail.itsligo.ie>
2017-11-10 12:04:40 +0000

    Lab2Changes

---
+++ Week6Lab/Models/ClubModels/ClubsDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Rad2016SampleWepApp.Models.ClubModels;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Week6Lab.Models.ClubModels
+{
+    public class ClubsDbContext : DbContext
+    {
+        public DbSet<Club> Clubs { get; set; }
+        public DbSet<Member> Members { get; set; }
+        public DbSet<ClubEvent> ClubEvents { get; set; }
+
+        public ClubsDbContext()
+            : base("DefaultConnection")
+        {
+
+        }
+    }
+}
